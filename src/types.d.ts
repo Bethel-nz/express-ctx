4e841905e18a786b02b0e05fd8f0b43941c14857
bethel. <71951344+Bethel-nz@users.noreply.github.com>
2024-09-08 13:18:56 +0100

    feat: updated types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,13 @@
-type AllowedValueTypes = string | number | boolean | Date | null | undefined;
+export type AllowedValueTypes =
+  | string
+  | number
+  | boolean
+  | Date
+  | null
+  | undefined
+  | Record<string, unknown>;
 
-interface MyContextOptions<T extends Record<string, AllowedValueTypes>> {
+export interface MyContextOptions<T extends Record<string, AllowedValueTypes>> {
   expiry?: number;
   defaultValues?: T;
   lazy?: boolean;
